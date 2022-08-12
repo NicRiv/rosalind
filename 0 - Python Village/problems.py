32fed4c62d2a71474e16502851262507aebfe92e
NicRiv <nico.a.rivera3@gmail.com>
2022-08-12 15:38:39 -0300

    New Python exercise added

--- 0 - Python Village/problems.py
+++ 0 - Python Village/problems.py
@@ -11,10 +11,11 @@ Return: The integer corresponding to the square of the hypotenuse of the right t
         whose legs have lengths a and b. 
 """
 print('\n', 'Exercise two')
+
 a = 941
 b = 893
-result2 = a**2 + b**2
-print(result2)
+result_2 = a**2 + b**2
+print(result_2)
 
 #3 - Strings and Lists
 """
@@ -25,13 +26,14 @@ Return: The slice of this string from indices a through b and c through d (with 
         inclusively. In other words, we should include elements s[b] and s[d] in our slice.
 """
 print('\n', 'Exercise three')
+
 Ai = 60
 Af = 62
 Bi = 83
 Bf = 94
 s = 'n6gU8Zp0B1I3k2D4McgObJM3DNfobLJ1XM16iAVgNBk7cIG4SZ8CxXglbxwxBoarebVP6YotwtoZaZSHfAQtaezanowskyiugXLLvIMafK1tbsKbPfVn3ERgsHd9nXwcx2RMmKmtmqwPC3hqwTwQDglwO7G0WVpomPL6SZq.'
-result3 = s[Ai:Af+1] + ' ' + s[Bi:Bf+1]
-print('-',result3,'-')
+result_3 = s[Ai:Af+1] + ' ' + s[Bi:Bf+1]
+print('-',result_3,'-')
 
 #4 - Conditions and Loops
 """
@@ -41,6 +43,7 @@ Given: Two positive integers a and b (a<b<10000).
 Return: The sum of all odd integers from a through b, inclusively.
 """
 print('\n', 'Exercise four')
+
 NUMBER_A = 4434
 NUMBER_B = 8671
 sum_of_odd_numbers = 0
@@ -50,3 +53,35 @@ for i in range(NUMBER_A, NUMBER_B + 1):
         sum_of_odd_numbers += i
 
 print(sum_of_odd_numbers)
+
+#5 - Working With Files
+"""
+Problem
+Given: A file containing at most 1000 lines.
+
+Return: A file containing all the even-numbered lines from the original file. 
+        Assume 1-based numbering of lines.
+"""
+print('\n', 'Exercise five')
+
+path = 'd://github//rosalind//0 - Python Village//'
+archivo = path + 'rosalind_ini5.txt'
+
+par = 0
+
+texto = []
+
+# Read Text
+with open(archivo, 'r') as file:
+    for line in file:
+        par += 1
+        if (par % 2 == 0):
+            # print(line.strip())
+            texto.append(line.strip())
+
+# Write a New Text
+with open(path + 'salida_ej5.txt', 'w') as file2:
+    for i in texto:
+        file2.write(str(i) + '\n')
+
+print('DONE')
