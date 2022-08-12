297bcc19d43e44fe2dc8484ebd2b18e72a413d65
NicRiv <nico.a.rivera3@gmail.com>
2022-08-12 12:59:07 -0300

    first exercise

---
+++ 0 - Python Village/problems.py
@@ -0,0 +1,27 @@
+#1 - Installing Python
+print('Exercise one')
+import this
+
+#2 - Variables and Some Arithmetic
+"""
+Problem:
+Given: Two positive integers a and b, each less than 1000.
+
+Return: The integer corresponding to the square of the hypotenuse of the right triangle 
+        whose legs have lengths a and b. 
+"""
+print('\n', 'Exercise two')
+a = 941
+b = 893
+result2 = a**2 + b**2
+print(result2)
+
+#3 - Strings and Lists
+"""
+Problem:
+Given: A string s of length at most 200 letters and four integers a, b, c and d.
+
+Return: The slice of this string from indices a through b and c through d (with space in between), 
+        inclusively. In other words, we should include elements s[b] and s[d] in our slice.
+"""
+print('\n', 'Exercise three')
